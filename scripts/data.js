101a4f6d1bbe0552df5af62855c7a0adc8e880f0
simon <scriado.f@gmail.com>
2022-04-26 13:12:01 +0200

    Added functionality to populate 3 tables at once, added error messages and refactored the focus current time functionality

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,36 @@
 const key = "VPpWII3CwC1th0Y19spcK9848kRBTAj68nRPjUW3";
 const cityForm = document.querySelector("form");
 
+// Small logic to check if the table is populated. If not we display a message for the user to search for a city
+const tbodyHoy = document.querySelector(".hoy");
+const container2 = document.querySelector(".container2");
+const containerSearch = document.querySelector(".containerSearch");
+const containerError = document.querySelector(".containerError");
+if (!tbodyHoy.innerHTML) {
+  containerSearch.classList.remove("d-none");
+  container2.classList.add("d-none");
+}
+
+// Fetch para saber cuantas consultas hemos hecho / nos quedan
+fetch("https://api.meteo.cat/quotes/v1/consum-actual", {
+  method: "GET",
+  headers: {
+    "X-API-key": key,
+  },
+})
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(function (json) {
+    console.log(json);
+  })
+  .catch(function (error) {
+    console.log("Request failed: " + error.message);
+  });
+
 // First we fetch all "cities"
 fetch("https://api.meteo.cat/referencia/v1/municipis", {
   method: "GET",
@@ -28,21 +58,36 @@ const searchForm = (municipios) => {
     // Prevent default action
     e.preventDefault();
 
+    // We display in the HTML template the city name we have searched
     const cityName = document.querySelector(".city");
 
-    // Get city value
+    // Get search value
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    // We loop through all cities and if the name is equal to the search input we fire the fetchPrediccionMunicipio (pasing the city code)
+    // We loop through all cities and if the cityName is equal to the search input we fire the fetchPrediccionMunicipio (pasing the city code)
+    // If the search does not work we display the error message
     for (const key in municipios) {
-      if (municipios[key].nom.toLowerCase() === city.toLowerCase()) {
+      if (municipios[key].nom.trim().toLowerCase() === city.toLowerCase()) {
+        containerError.classList.add("d-none");
         cityName.innerHTML = municipios[key].nom;
         codigoMunicipio = municipios[key].codi;
+        console.log(`1: ${municipios[key].nom} & ${municipios[key].codi}`);
         fetchPrediccionMunicipio(codigoMunicipio);
+      } else {
+        containerSearch.classList.add("d-none");
+        containerError.classList.remove("d-none");
       }
     }
+    // Set local storage
+    localStorage.setItem("codigoMunicipio", codigoMunicipio);
   });
+  if (localStorage.getItem("codigoMunicipio")) {
+    localStorage
+      .getItem("codigoMunicipio")
+      .then((data) => fetchPrediccionMunicipio(data))
+      .catch((err) => console.log(err));
+  }
 };
 
 // In this function we fetch the forecast for a specific city (as we get the city code from the prev function)
@@ -64,6 +109,13 @@ const fetchPrediccionMunicipio = (codigoMunicipio) => {
     })
     .then(function (json) {
       prediccionMunicipio = json;
+      console.log(`2: we are fetching ${codigoMunicipio}`);
+      console.log(prediccionMunicipio);
+      // Once we have the specific city forecast we fire the function to populate the table
+      // We also hide the search and error messages and show the table container
+      containerSearch.classList.add("d-none");
+      containerError.classList.add("d-none");
+      container2.classList.remove("d-none");
       dataTable(prediccionMunicipio);
     })
     .catch(function (error) {
@@ -72,49 +124,80 @@ const fetchPrediccionMunicipio = (codigoMunicipio) => {
 };
 
 const dataTable = (prediccionMunicipio) => {
-  const tbody = document.querySelector("tbody");
-  const today = prediccionMunicipio.dies[0].variables;
+  // We get the 3 different tables
+  const tbodyHoy = document.querySelector(".hoy");
+  const tbodyMañana = document.querySelector(".mañana");
+  const tbodyPasadoMañana = document.querySelector(".pasadoMañana");
 
-  // Today´s table
-  const trSky = document.createElement("tr");
-  const th1 = document.createElement("th");
-  th1.innerHTML = "Cielo";
-  trSky.append(th1);
-  for (const key in today.estatCel.valors) {
-    const td1 = document.createElement("td");
-    td1.innerHTML = today.estatCel.valors[key].valor;
-    trSky.append(td1);
+  // We reset the t-bodies in case the user searches a new city. Otherwise the tables get duplicated (or more...)
+  tbodyHoy.innerHTML = "";
+  tbodyMañana.innerHTML = "";
+  tbodyPasadoMañana.innerHTML = "";
+
+  // We separate the variables for the 3 days
+  const forecastDays = prediccionMunicipio.dies;
+  const threeDays = [
+    forecastDays[0].variables,
+    forecastDays[1].variables,
+    forecastDays[2].variables,
+  ];
+  console.log(threeDays.length);
+  // Error: Vigilar también si el nombre tiene tildes o simbolos
+
+  // Loop to create the 3 tables at once
+  for (i = 0; i < threeDays.length; i++) {
+    const trSky = document.createElement("tr");
+    const th1 = document.createElement("th");
+    th1.innerHTML = "Cielo";
+    trSky.append(th1);
+    for (const key in threeDays[i].estatCel.valors) {
+      const td1 = document.createElement("td");
+      td1.innerHTML = threeDays[i].estatCel.valors[key].valor;
+      trSky.append(td1);
+    }
+
+    const trTemp = document.createElement("tr");
+    const th2 = document.createElement("th");
+    th2.innerHTML = `Temp (${threeDays[i].temp.unitat})`;
+    trTemp.append(th2);
+    for (const key in threeDays[i].temp.valors) {
+      const td2 = document.createElement("td");
+      td2.innerHTML = threeDays[i].temp.valors[key].valor;
+      trTemp.append(td2);
+    }
+
+    const trRain = document.createElement("tr");
+    const th3 = document.createElement("th");
+    th3.innerHTML = "Lluvia (mm)";
+    trRain.append(th3);
+    for (const key in threeDays[i].precipitacio.valor) {
+      const td3 = document.createElement("td");
+      td3.innerHTML = threeDays[i].precipitacio.valor[key].valor;
+      trRain.append(td3);
+    }
+
+    const trWind = document.createElement("tr");
+    const th4 = document.createElement("th");
+    th4.innerHTML = "Viento (km/h)";
+    trWind.append(th4);
+    for (const key in threeDays[i].velVent.valors) {
+      const td4 = document.createElement("td");
+      td4.innerHTML = threeDays[i].velVent.valors[key].valor;
+      trWind.append(td4);
+    }
+
+    // If the i=0 we populate todays table, if 1 we populate tomorrows table, else we populate day after tomorrows table
+    if (i === 0) {
+      tbodyHoy.append(trSky, trTemp, trRain, trWind);
+    } else if (i === 1) {
+      tbodyMañana.append(trSky, trTemp, trRain, trWind);
+    } else {
+      tbodyPasadoMañana.append(trSky, trTemp, trRain, trWind);
+    }
   }
 
-  const trTemp = document.createElement("tr");
-  const th2 = document.createElement("th");
-  th2.innerHTML = `Temp ${today.temp.unitat}`;
-  trTemp.append(th2);
-  for (const key in today.temp.valors) {
-    const td2 = document.createElement("td");
-    td2.innerHTML = today.temp.valors[key].valor;
-    trTemp.append(td2);
-  }
-
-  const trRain = document.createElement("tr");
-  const th3 = document.createElement("th");
-  th3.innerHTML = "Lluvia (mm)";
-  trRain.append(th3);
-  for (const key in today.precipitacio.valor) {
-    const td3 = document.createElement("td");
-    td3.innerHTML = today.precipitacio.valor[key].valor;
-    trRain.append(td3);
-  }
-
-  const trWind = document.createElement("tr");
-  const th4 = document.createElement("th");
-  th4.innerHTML = "Viento (km/h)";
-  trWind.append(th4);
-  for (const key in today.velVent.valors) {
-    const td4 = document.createElement("td");
-    td4.innerHTML = today.velVent.valors[key].valor;
-    trWind.append(td4);
-  }
-
-  tbody.append(trSky, trTemp, trRain, trWind);
+  // vigilar esto, creo que ya no hace falta
+  tables.forEach((t) => {
+    t.classList.remove("d-none");
+  });
 };
--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,4 +1,3 @@
-const focus = document.querySelector(".focus");
 const clockNode = document.querySelector("#clock");
 const dateNode = document.querySelector("#date");
 
@@ -10,10 +9,10 @@ const movingFocus = () => {
   // Every second we fire this code
   return setInterval(() => {
     // Every second we get the numeric value of the bar width
-    const timeLine = document.querySelector(".timeLine");
-    const timeLineStyle = getComputedStyle(timeLine);
-    const timeLineWidth = timeLineStyle.width;
-    const numeric = parseInt(timeLineWidth);
+    // const timeLine = document.querySelector(".timeLine");
+    // const timeLineStyle = getComputedStyle(timeLine);
+    // const timeLineWidth = timeLineStyle.width;
+    // const numeric = parseInt(timeLineWidth);
 
     // We get the current todays date and format it to display the time
     let actualDate = new Date();
@@ -25,22 +24,33 @@ const movingFocus = () => {
     }-${actualDate.getFullYear()}`;
     dateNode.textContent = date;
 
+    currentHour = actualDate.getHours();
+
+    const todayTableHours = document.querySelectorAll(".todayHours");
+    const todayTable = document.querySelector(".hoy");
+    todayTableHours.forEach((hour) => {
+      hour.classList.remove("focus");
+      if (hour.innerText == `${currentHour}h` && todayTable.innerHTML != "") {
+        hour.classList.add("focus");
+      }
+    });
+
     // We calculate the amount of seconds between 00:00:00 and the current time
-    let diffMilliseconds = actualDate - lastMidnight;
-    var diffSeconds = Math.floor(
-      ((diffMilliseconds % 86400000) / 3600000) * 3600
-    );
+    // let diffMilliseconds = actualDate - lastMidnight;
+    // var diffSeconds = Math.floor(
+    //   ((diffMilliseconds % 86400000) / 3600000) * 3600
+    // );
 
     // If the amount of seconds is 86400 (24h) we reset the position of the focus to start again on the left side of the bar
-    if (diffSeconds === 86400) {
-      marginLeft = 0;
-      // If the 86400 seconds are not yet reached we set the margin-left to be the amount of pixels of margin-left per second * the amount of minutes since midnight
-    } else {
-      // marginLeft is equal to the width in pixels divided by the amount of seconds a day has (this to know how many pixels does the margin-left need to
-      // move every second to complete the bar in 24h) multiplied by the seconds from midnight until now
-      marginLeft = (numeric / 86400) * diffSeconds;
-    }
-    focus.style.marginLeft = `${marginLeft}px`;
+    // if (diffSeconds === 86400) {
+    //   marginLeft = 0;
+    // If the 86400 seconds are not yet reached we set the margin-left to be the amount of pixels of margin-left per second * the amount of minutes since midnight
+    // } else {
+    // marginLeft is equal to the width in pixels divided by the amount of seconds a day has (this to know how many pixels does the margin-left need to
+    // move every second to complete the bar in 24h) multiplied by the seconds from midnight until now
+    //   marginLeft = (numeric / 86400) * diffSeconds;
+    // }
+    // focus.style.marginLeft = `${marginLeft}px`;
   }, 1000);
 };
 
